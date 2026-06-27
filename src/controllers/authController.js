4bb3ff65c6ef6b305eb9eacdcf47cb39093a9876
Rostik <rosticmel@gmail.com>
2026-06-27 12:29:32 +0300

    Fix auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,116 @@
-import { Joi, Segments } from 'celebrate';
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 
-export const registerUserSchema = {
-  [Segments.BODY]: Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required(),
-  }),
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none',
 };
 
-export const loginUserSchema = {
-  [Segments.BODY]: Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }),
+export const registerUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw createHttpError(400, 'Email in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    const session = await createSession(user._id);
+
+    setSessionCookies(res, session);
+
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw createHttpError(401, 'Invalid credentials');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw createHttpError(401, 'Invalid credentials');
+    }
+
+    await Session.deleteOne({ userId: user._id });
+
+    const session = await createSession(user._id);
+
+    setSessionCookies(res, session);
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshUserSession = async (req, res, next) => {
+  try {
+    const { sessionId, refreshToken } = req.cookies;
+
+    const session = await Session.findOne({
+      _id: sessionId,
+      refreshToken,
+    });
+
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    if (new Date() > session.refreshTokenValidUntil) {
+      throw createHttpError(401, 'Session token expired');
+    }
+
+    await Session.deleteOne({ _id: session._id });
+
+    const newSession = await createSession(session.userId);
+
+    setSessionCookies(res, newSession);
+
+    res.status(200).json({
+      message: 'Session refreshed',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logoutUser = async (req, res, next) => {
+  try {
+    const { sessionId } = req.cookies;
+
+    if (sessionId) {
+      await Session.deleteOne({ _id: sessionId });
+    }
+
+    res.clearCookie('accessToken', cookieOptions);
+    res.clearCookie('refreshToken', cookieOptions);
+    res.clearCookie('sessionId', cookieOptions);
+
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
